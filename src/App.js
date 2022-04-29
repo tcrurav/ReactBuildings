a4c1ac8a3b6d697a12a5e08048b9aa36fddcddb0
tcrurav <tiburcio.cruz@gmail.com>
2022-04-29 15:38:06 +0100

    For Jose Iván

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import {
 import Fruits from './pages/Fruits';
 import BuildingList from './pages/BuildingList';
 import Architects from './pages/Architects';
+import ChangingPage from './pages/ChangingPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/architects" element={<Architects />} />
+        <Route path="/changing-page" element={<ChangingPage />} />
+        <Route path="/architects" element={<Architects />} />
         <Route path="/building-list" element={<BuildingList />} />
         <Route path="/fruits" element={<Fruits />} />
         <Route path="/type-list" element={<BuildingTypeList />} />
         <Route path="/home" element={<Home />} />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<ChangingPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/ChangingPage.js
@@ -0,0 +1,11 @@
+import "./ChangingPage.css";
+
+export default function ChangingPage(){
+  return (
+    <>
+      <div className="changing-page-container">
+        <button>change background color</button>
+      </div>
+    </>
+  );
+}
